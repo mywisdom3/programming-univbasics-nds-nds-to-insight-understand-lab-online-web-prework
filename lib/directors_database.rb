4e1784b068d6733d59425d08c04289d006716eb2
bionic-chain-9657 <mywisdom3@yahoo.com>
2019-12-01 21:43:48 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -1,6 +1,13 @@
 require 'yaml'
-
+require pp
 def directors_database
-	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
+  rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
+  @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
+end
+
+require 'pp'
+require pp
+def directors_database
+  rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
 end
